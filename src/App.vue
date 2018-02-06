f4013f1716d677d48718dfa0a6071379e1dff493
Marcian Diamond <marciandmnd@yahoo.com>
2018-02-05 21:30:57 -0800

    JavaScript animation example

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,52 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Animations</h1>
                 <hr>
+                <select v-model='alertAnimation' class="form-control">
+                    <option value="fade">Fade</option>
+                    <option value="slide">Slide</option>
+                </select>
+                <br/>
                 <button @click="show =!show" class="btn btn-primary">Show alert</button>
                 <br>
                 <br>
-                <transition name="fade">
+                <transition :name="alertAnimation" mode="out-in">
+                    <div v-if="show" key="info" class="alert alert-info">Some info</div>
+                    <div v-else key="warning" class="alert alert-warning">Some warning</div>
+                </transition>
+                <transition :name="alertAnimation">
+                    <div v-show="show" class="alert alert-info">Some info</div>
+                </transition>
+                <transition name="slide" type="animation" appear>
                     <div v-if="show" class="alert alert-info">Some info</div>
                 </transition>
-                <transition name="slide">
+                <transition name="fade" appear>
                     <div v-if="show" class="alert alert-info">Some info</div>
+                </transition>
+
+                <transition
+                    enter-class=""
+                    enter-active-class="animated bounce"
+                    leave-class=""
+                    leave-active-class="animated shake" >
+                    <div v-if="show" class="alert alert-success">Some info</div>
+                </transition>
+                <hr>
+                <h2>JavaScript Animations</h2> 
+                <button class="btn btn-primary" @click="load = !load">Load / Remove Element</button>
+                <br>
+                <br>
+                <transition
+                    @before-enter="beforeEnter"
+                    @enter="enter"
+                    @after-enter="afterEnter"
+                    @enter-cancelled="enterCancelled"
+                    
+                    @before-leave="beforeLeave"
+                    @leave="leave"
+                    @after-leave="afterLeave"
+                    @leave-cancelled="leaveCancelled"
+                    :css="false">
+                    <div v-if="load" style="width: 300px; height: 100px; background-color: lightgreen;"></div>
                 </transition>
             </div>
         </div>
@@ -22,7 +60,60 @@
     export default {
         data() {
             return {
-                show: false
+                show: false,
+                load: true,
+                alertAnimation: 'fade',
+                elementWidth: 100
+            }
+        },
+        methods: {
+            beforeEnter(el) {
+                console.log('beforeEnter')
+                this.elementWidth = 100;
+                el.style.width = this.elementWidth + 'px';
+            },
+            enter(el, done) {
+                console.log('enter')
+                let round = 1;
+                const interval = setInterval(() => {
+                    el.style.width = (this.elementWidth + round * 10) + 'px'
+                    round++
+                    if (round > 20) {
+                        clearInterval(interval)
+                        done()
+                    }
+                }, 20);
+            },
+            afterEnter(el) {
+                console.log('aferEnter')
+            },
+            enterCancelled(el) {
+                console.log('enterCancelled')
+            },
+            beforeLeave(el) {
+                console.log('beforeLeave')
+                
+                this.elementWidth = 300
+                el.style.width = this.elementWidth + 'px';
+            },
+            leave(el, done) {
+                console.log('leave')
+                let round = 1;
+                const interval = setInterval(() => {
+                    el.style.width = (this.elementWidth - round * 10) + 'px'
+                    round++
+                    if (round > 20) {
+                        clearInterval(interval)
+                        done()
+                    }
+                }, 20);
+            },
+            afterLeave(el) {
+                console.log('afterLeave')
+
+            },
+            leaveCancelled(el) {
+                console.log('leaveCancelled');
             }
         }
     }
@@ -46,9 +137,10 @@
     }
 
     .slide-enter {
-
+        opacity: 0;
     }
     .slide-enter-active {
+        transition: opacity .5s;
         animation: slide-in 1s ease-out forwards;
     }
     .slide-leave {
@@ -56,6 +148,8 @@
     }
     .slide-leave-active {
         animation: slide-out 1s ease-out forwards;
+        transition: opacity 1s;
+        opacity: 0;
     }
 
     @keyframes slide-in {
